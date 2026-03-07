7eacbc85fbb4983ab6f676ae52e8912dc59a2ac6
TEJAS R <tejasmr05@gmail.com>
2026-03-07 23:55:25 +0530

    search.py updated

--- app/search.py
+++ app/search.py
@@ -1,26 +1,28 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+
 
 def search_recipe(dish_name):
 
-    query = dish_name.replace(" ","+")
-
+    query = dish_name.replace(" ", "+")
     url = f"https://www.allrecipes.com/search?q={query}"
 
     headers = {
-        'User-Agent':'Mozilla/5.0'
+        "User-Agent": "Mozilla/5.0"
     }
 
     response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    link = soup.select_one("a[href*='/recipe/']")
+    # Find all links
+    links = soup.find_all("a", href=True)
 
-    if link:
-        return link["href"]
-    
+    for link in links:
+        href = link["href"]
+
+        if "/recipe/" in href:
+            return href
+
     return None
 
-
-#git is changed
