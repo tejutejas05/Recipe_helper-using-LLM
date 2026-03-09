fd6bb579383a7ba7a6538879c1bc2851efbe9c43
TEJAS R <tejasmr05@gmail.com>
2026-03-09 22:55:44 +0530

    test.py

--- app/test.py
+++ app/test.py
@@ -1,15 +1,25 @@
 from recipe_scrapers import scrape_me
 
-url = "https://www.allrecipes.com/recipe/16102/chicken-biryani/"
+urls = [
+    "https://www.allrecipes.com/recipe/16102/chicken-biryani/",
+    "https://www.allrecipes.com/recipe/46822/indian-chicken-curry/",
+    "https://www.allrecipes.com/recipe/24706/gulab-jamun/"
+]
 
-scraper = scrape_me(url)
+recipes = []
 
-recipe = {
-    "name": scraper.title(),
-    "ingredients": scraper.ingredients(),
-    "instructions": scraper.instructions(),
-    "servings": scraper.yields(),
-    "total_time": scraper.total_time()
-}
+for url in urls:
 
-print(recipe)
+    scraper = scrape_me(url)
+
+    recipe = {
+        "name": scraper.title(),
+        "ingredients": scraper.ingredients(),
+        "instructions": scraper.instructions(),
+        "servings": scraper.yields(),
+        "total_time": scraper.total_time()
+    }
+
+    recipes.append(recipe)
+
+print(recipes)
